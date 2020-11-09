2e772b323589c459eb410aa4916565927f079bfe
tifaine <tifaine.guyot@gmail.com>
2020-11-09 18:01:38 +0100

    Initialization of main.cpp and main.qml

--- main.cpp
+++ main.cpp
@@ -1,20 +1,37 @@
-#include <QGuiApplication>
-#include <QQmlApplicationEngine>
+#include <QtWidgets/QApplication>
+#include <QtQuick/QQuickView>
+#include <QtQml/QQmlEngine>
+#include <QQmlContext>
 
 int main(int argc, char *argv[])
 {
-    QCoreApplication::setAttribute(Qt::AA_EnableHighDpiScaling);
+    //We use charts so we must use QApplication.
+    QApplication app(argc, argv);
+    app.setOrganizationName("RCO");
+    app.setOrganizationDomain("RCO");
+    // The following are needed to make examples run without having to install the module
+    // in desktop environments.
 
-    QGuiApplication app(argc, argv);
+    QQuickView viewer;
+    QString extraImportPath(QStringLiteral("%1/../../../%2"));
 
-    QQmlApplicationEngine engine;
-    const QUrl url(QStringLiteral("qrc:/main.qml"));
-    QObject::connect(&engine, &QQmlApplicationEngine::objectCreated,
-                     &app, [url](QObject *obj, const QUrl &objUrl) {
-        if (!obj && url == objUrl)
-            QCoreApplication::exit(-1);
-    }, Qt::QueuedConnection);
-    engine.load(url);
+    viewer.engine()->addImportPath(extraImportPath.arg(QGuiApplication::applicationDirPath(),
+                                                       QString::fromLatin1("qml")));
+    QObject::connect(viewer.engine(), &QQmlEngine::quit, &viewer, &QWindow::close);
+
+    viewer.setTitle(QStringLiteral("Creator Echalote"));
+#ifdef Q_OS_LINUX
+    viewer.engine()->rootContext()->setContextProperty("fileRoot", "file://");
+
+#elif defined(Q_OS_WIN)
+    viewer.engine()->rootContext()->setContextProperty("fileRoot", "file:///");
+
+#endif
+    viewer.engine()->rootContext()->setContextProperty("applicationDirPath", QGuiApplication::applicationDirPath());
+
+    viewer.setSource(QUrl("qrc:/main.qml"));
+    viewer.setResizeMode(QQuickView::SizeRootObjectToView);
+    viewer.show();
 
     return app.exec();
 }
--- main.qml
+++ main.qml
@@ -1,9 +1,47 @@
 import QtQuick 2.15
 import QtQuick.Window 2.15
+import QtGraphicalEffects 1.0
 
-Window {
-    width: 640
-    height: 480
-    visible: true
-    title: qsTr("Hello World")
+Item {
+    width: 1600
+    height: 800
+    Rectangle
+    {
+        //Creation of a custom wallpaper
+        id: rectangle
+        anchors.fill: parent
+        LinearGradient {
+            anchors.right: parent.right
+            anchors.rightMargin: parent.width/2
+            anchors.left: parent.left
+            anchors.leftMargin: 0
+            anchors.bottom: parent.bottom
+            anchors.bottomMargin: 0
+            anchors.top: parent.top
+            anchors.topMargin: 0
+            start: Qt.point(0, 0)
+            end: Qt.point(parent.width/2, parent.height)
+            gradient: Gradient {
+                GradientStop { position: 0.0; color: "#0d0d0d" }
+                GradientStop { position: 1.0; color: "#262626" }
+            }
+        }
+
+        LinearGradient {
+            anchors.right: parent.right
+            anchors.rightMargin: 0
+            anchors.left: parent.left
+            anchors.leftMargin: parent.width/2
+            anchors.bottom: parent.bottom
+            anchors.bottomMargin: 0
+            anchors.top: parent.top
+            anchors.topMargin: 0
+            start: Qt.point(parent.width/2, 0)
+            end: Qt.point(0, parent.height)
+            gradient: Gradient {
+                GradientStop { position: 0.0; color: "#0d0d0d" }
+                GradientStop { position: 1.0; color: "#262626" }
+            }
+        }
+    }
 }
