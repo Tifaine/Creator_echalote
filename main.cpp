ae4867fc74c390464f73c35078ed9df6cda52458
tifaine <tifaine.guyot@gmail.com>
2020-11-09 18:10:46 +0100

    Add a function to create all required repository

--- main.cpp
+++ main.cpp
@@ -2,6 +2,9 @@
 #include <QtQuick/QQuickView>
 #include <QtQml/QQmlEngine>
 #include <QQmlContext>
+#include <QtCore/QDir>
+
+void initRep();
 
 int main(int argc, char *argv[])
 {
@@ -9,6 +12,8 @@ int main(int argc, char *argv[])
     QApplication app(argc, argv);
     app.setOrganizationName("RCO");
     app.setOrganizationDomain("RCO");
+
+    initRep();
     // The following are needed to make examples run without having to install the module
     // in desktop environments.
 
@@ -34,4 +39,40 @@ int main(int argc, char *argv[])
     viewer.show();
 
     return app.exec();
+}
+
+//Add a function to initialize all repository we will have to use.
+void initRep()
+{
+    if( ! QDir("data").exists())
+    {
+        QDir().mkdir("data");
+        QDir().mkdir("data/Dyna");
+        QDir().mkdir("data/Etape");
+        QDir().mkdir("data/Sequence");
+        QDir().mkdir("data/Actions");
+        QDir().mkdir("data/Strategie");
+    }else
+    {
+        if( ! QDir("data/Dyna").exists())
+        {
+            QDir().mkdir("data/Dyna");
+        }
+        if( ! QDir("data/Etape").exists())
+        {
+            QDir().mkdir("data/Etape");
+        }
+        if( ! QDir("data/Sequence").exists())
+        {
+            QDir().mkdir("data/Sequence");
+        }
+        if( ! QDir("data/Actions").exists())
+        {
+            QDir().mkdir("data/Actions");
+        }
+        if( ! QDir("data/Strategie").exists())
+        {
+            QDir().mkdir("data/Strategie");
+        }
+    }
 }
